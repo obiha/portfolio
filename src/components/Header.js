7944b4596ae295577acc2b54c30aaba1ef4b60b9
Joshua Obiha <joshua.obiha@hotmail.com>
2022-12-02 01:01:15 -0500

    initial-page

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+          <h6 style={{color: 'white', fontSize: "7rem", }} class="display-4">[@joshua_obiha]</h6>
+        </header>
+
+        
+
+    )
+}
+
+
+
+
+export default Header
---
+++ src/components/Navigation.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const Navigation = () => {
+    return (
+        <div style={{
+            backgroundColor: '#2B3D5B', 
+            height: '3.6em', 
+            width: '100%', 
+            borderRadius: '9px',
+            display: 'flex',
+            // justifyContent: 'center',
+            // alignItems: 'center',
+            // top: "80px"
+            }}>
+
+            <div style={{
+                margin: "4px",
+                backgroundColor: "white", 
+                height: "3em", 
+                width: "6em", 
+                borderRadius: "5px", 
+                textAlign: "center",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                cursor: "pointer"
+                }}>
+           
+            </div>
+
+            {/* <div style={{
+                margin: "4px",
+                backgroundColor: "white", 
+                height: "3em", 
+                width: "6em", 
+                borderRadius: "5px", 
+                textAlign: "center",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                cursor: "pointer"
+                }}>
+                    About
+            </div>
+
+            <div style={{
+                margin: "4px",
+                backgroundColor: "white", 
+                height: "3em", 
+                width: "6em", 
+                borderRadius: "5px", 
+                textAlign: "center",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                cursor: "pointer"
+                }}>
+                    Projects
+            </div>
+
+            <div style={{
+                margin: "4px",
+                backgroundColor: "white", 
+                height: "3em", 
+                width: "6em", 
+                borderRadius: "5px", 
+                textAlign: "center",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                cursor: "pointer"
+                }}>
+                    Contact
+            </div>
+
+            <div style={{
+                margin: "4px",
+                backgroundColor: "white", 
+                height: "3em", 
+                width: "6em", 
+                borderRadius: "5px", 
+                textAlign: "center",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                cursor: "pointer"
+                }}>
+           
+            </div> */}
+
+            
+
+            
+        </div>
+
+    )
+}
+
+
+
+
+export default Navigation
