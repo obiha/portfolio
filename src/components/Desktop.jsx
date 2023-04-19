6aff623b884e4315506e834f0440b192180f5f4e
Joshua Obiha <joshua.obiha@hotmail.com>
2023-04-19 14:49:51 -0400

    New layout

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -19,7 +19,7 @@ const Desktop = () => {
 
                 const myHeaders = new Headers();
                 myHeaders.append("Content-Type", "application/json");
-                myHeaders.set('Authorization', 'Basic ' + btoa('8edcb3860cd33ee0733d8f9604fd4333'+":" +'bb20894356ad788e6a563b9eba35fc36'));
+                myHeaders.set('Authorization', 'Basic ' + btoa('8edcb3860cd33ee0733d8f9604fd4333' + ":" + 'bb20894356ad788e6a563b9eba35fc36'));
 
                 const data = JSON.stringify({
                         "Messages": [{
@@ -43,9 +43,9 @@ const Desktop = () => {
                         method: 'POST',
                         headers: myHeaders,
                         body: data,
-                      };
-                    
-                      fetch("https://api.mailjet.com/v3.1/send", requestOptions)
+                };
+
+                fetch("https://api.mailjet.com/v3.1/send", requestOptions)
                         .then(response => response.text())
                         .then(result => console.log(result))
                         .catch(error => console.log('error', error));
@@ -54,7 +54,7 @@ const Desktop = () => {
 
 
         return (
-                <div style={{ position: "absolute", backgroundColor: '#FAF9F6', height: '500%', width: '100%', marginTop: "0%", marginLeft: "0%", borderColor: '#FF0000' }}>
+                <div style={{ position: "absolute", backgroundColor: '#FAF9F6', width: '100%', marginTop: "0%", marginLeft: "0%", borderColor: '#FF0000' }}>
                         <div style={{ top: '0', position: 'sticky', width: "100%", backgroundColor: '#FAF9F6', display: 'flex', justifyContent: 'right', marginTop: '0%', padding: '20px', zIndex: '19', }}>
                                 <motion.div style={{ marginleft: '2%', marginRight: '2%', marginTop: '0.2%', cursor: 'pointer', verticalAlign: 'middle', justifyContent: 'center', alignItems: 'center' }}
                                         whileHover={{ scale: 1.1, rotate: 0, color: '#000000' }}
@@ -64,8 +64,8 @@ const Desktop = () => {
                                         whileHover={{ scale: 1.1, rotate: 0, color: '#000000' }}><a style={{ verticalAlign: 'middle' }}>About</a></motion.div>
                                 <motion.div style={{ marginleft: '2%', marginRight: '2%', marginTop: '0.2%', cursor: 'pointer', verticalAlign: 'middle' }}
                                         whileHover={{ scale: 1.1, rotate: 0, color: '#000000' }}> <a style={{ verticalAlign: 'middle' }}>Skills</a></motion.div>
-                                <motion.div style={{ marginleft: '2%', marginRight: '2%', marginTop: '0.2%', cursor: 'pointer', verticalAlign: 'middle' }}
-                                        whileHover={{ scale: 1.1, rotate: 0, color: '#000000' }}><a style={{ verticalAlign: 'middle' }}>Services</a></motion.div>
+                                {/* <motion.div style={{ marginleft: '2%', marginRight: '2%', marginTop: '0.2%', cursor: 'pointer', verticalAlign: 'middle' }}
+                                        whileHover={{ scale: 1.1, rotate: 0, color: '#000000' }}><a style={{ verticalAlign: 'middle' }}>Services</a></motion.div> */}
                                 <motion.div style={{ marginleft: '2%', marginRight: '2%', marginTop: '0.2%', cursor: 'pointer', verticalAlign: 'middle' }}
                                         whileHover={{ scale: 1.1, rotate: 0, color: '#000000' }}><a style={{ verticalAlign: 'middle' }}>Portfolio</a></motion.div>
                                 <motion.div style={{ marginleft: '2%', marginRight: '2%', marginTop: '0.2%', cursor: 'pointer', verticalAlign: 'middle' }}
@@ -82,44 +82,56 @@ const Desktop = () => {
 
                                 {/*Displays the greetings and Cool Button */}
                                 <motion.div
-                                        style={{ marginLeft: "5%", marginRight: "5%", marginTop: "5%", marginBottom: "5%", fontSize: "46px" }}
+                                        style={{ marginLeft: "5%", marginRight: "5%", marginTop: "2%", marginBottom: "2%", fontSize: "46px" }}
 
                                         whileInView={{ backgroundColor: "#FAF9F6", y: 0, transition: { type: "spring", bounce: 0.5, duration: 1.50 } }}
                                         viewport={{ once: true, amount: 0.8 }}
                                 >
-                                        Joshua Obiha
+                                        <div style={{ display: "flex", alignItems: "center", alignContent: "center", justifyContent: "center" }}>
+
+                                                <motion.button style={{ marginRight: "0%", marginBottom: "1%", marginTop: "2%", outline: 'none', border: 'none', width: '350px', height: '350px', borderRadius: '20%', background: '#FAF9F6', fontSize: "16px", color: "#FFFFFF" }}>
+
+                                                        <img src={require('../images/profile.jpg')} style={{ width: "98%", height: "97%", borderRadius: '20%', zIndex: '2', marginRight: '0%', marginLeft: '0%', marginTop: '0%', filter: "grayscale(100%)" }} />
+                                                </motion.button>
+                                        </div>
+
+                                        <div style={{ display: "flex", alignItems: "center", height: "50%", alignContent: "center", justifyContent: "center" }}>
+
+                                                Joshua Obiha
+                                        </div>
+
                                         {/* @joshuaobiha */}
                                         {/* <motion.div style={{ width: "1em", height: "1em", float: "right", marginRight: "4%", marginTop: "1%", borderRadius: '50%', textAlign: "center", background: "#000000" }}
                                                 whileHover={{ scale: 1.1, rotate: 0, }}
-                                                whileTap={{ scale: 0.9, background: "#FFFFFF" }} onClick={}>
+                                                whileTap={{ scale: 0.9, background: "#FFFFFF" }} >
                                         </motion.div> */}
 
 
-                                        {/* <motion.button style={{ float: "right", marginRight: "5%", marginTop: "0%", outline:'none', border: 'none', cursor: 'none', width:'200px', height:'200px', borderRadius:'20%', background:'#F1F5F5',fontSize: "16px" , color:"#FFFFFF"}}>
-                                                                          
-                                        <img src={require('../images/profile.jpg')} style={{width: "98%",height: "97%", borderRadius:'20%', zIndex:'2', marginRight:'0%', marginLeft:'0%', marginTop:'0%'}}/>
-                                        </motion.button> */}
+
 
 
 
                                 </motion.div>
 
                                 <motion.div
-                                        style={{ backgroundColor: "#FAF9F6", height: '22em', borderRadius: '9px', marginTop: "17%", marginLeft: "1%", marginRight: "1%", marginBottom: "3%", }}
+                                        style={{ backgroundColor: "#FAF9F6", height: '22em', borderRadius: '9px', marginTop: "2%", marginLeft: "1%", marginRight: "1%", marginBottom: "2%", }}
                                         whileInView={{ backgroundColor: "#FAF9F6", y: 0, transition: { type: "spring", bounce: 0.5, duration: 1.5 } }}
                                         viewport={{ once: true, amount: 0.8 }}
                                 >
-                                        <h1 class="display-1" style={{ marginLeft: "4%", paddingTop: "1%" }}>Hey!</h1>
+                                        {/* <h1 class="display-1" style={{ marginLeft: "4%", paddingTop: "1%" }}>Hey!</h1> */}
 
-                                        <p style={{ marginLeft: "4%", paddingTop: "2%", width: "60%", fontSize: "20px" }} class="text-left">
-                                                My name is Joshua Obiha, a recent Computer Science graudate. Currently exploring the intersection of Technology, Business and Design  and I'm seeking roles in Web Development</p>
+                                        <div style={{ display: "flex", alignItems: "center", alignContent: "center", justifyContent: "center"}}>
+                                                <p style={{width: "50%", fontSize: "20px", alignItems: "center", justifyContent: "center" }} class="text-left">
+                                                        I'm a recent Computer Science graduate with a passion for the intersection of technology and design. Currently seeking roles in web development & data analysis to further advance my skills and bring innovative web solutions to life</p>
 
-                                        <div style={{ marginBottom: "10em", marginLeft: "3%" }}>
-                                                <button type="button" class="btn btn-dark btn-secondary" data-toggle="tooltip" data-placement="top" title="Send Email" style={{ marginRight: '1%', marginLeft: '1%' }} onClick={() => window.location = 'mailto:joshua.obiha@hotmail.com'}>
+                                        </div>
+                                        <div style={{ display: "flex", alignItems: "center", marginTop: "1%"}}>
+                                        <button type="button" class="btn btn-dark btn-secondary" data-toggle="tooltip" data-placement="top" title="Send Email" style={{ marginRight: '1%', marginLeft: '41.5%' }} onClick={() => window.location = 'mailto:joshua.obiha@hotmail.com'}>
                                                         GET IN TOUCH</button>
                                                 {/* <button style={{ marginLeft: '1%', marginRight: '1%' }} type="button" class="btn btn-outline-dark">RESUME</button> */}
                                                 <a style={{ marginLeft: '1%', marginRight: '1%' }} type="button" href="https://drive.google.com/file/d/1Sn2xVLNpKZkFkYs1KCi8GemCwIn0pjm8/view?usp=sharing" class="btn btn-outline-dark">PDF Resume</a>
                                         </div>
+
                                 </motion.div>
 
                                 {/* Selected Work div to be made a component//////////////////////////////////////////////////////////////// */}
@@ -251,8 +263,8 @@ const Desktop = () => {
 
 
                         {/* Footer Should be made a component  */}
-                        <div style={{ backgroundColor: "#F8F9FA" }}>
-                                <div style={{ backgroundColor: "#F8F9FA", paddingBottom: "5%", paddingTop: "2%", textAlign: "center", marginBottom: "0" }}>
+                        {/* <div style={{ backgroundColor: "#808080" }}>
+                                <div style={{ backgroundColor: "#808080", paddingBottom: "0%", paddingTop: "0%", textAlign: "center", marginBottom: "0" }}>
                                         <a href="https://www.linkedin.com/in/joshuaobiha/" class="link-dark"> <LinkedInIcon color="#00b8ff" fontSize="large" style={{ marginRight: '1%', marginTop: '2%' }} /></a>
                                         <a href="https://github.com/obiha" class="link-dark"> <GitHubIcon color="#00b8ff" fontSize="large" style={{ marginRight: '1%', marginTop: '2%' }} /></a></div>
 
@@ -260,10 +272,11 @@ const Desktop = () => {
                                 <h4 style={{ marginLeft: "2%", paddingTop: "0%" }}>How I built this</h4>
                                 <div style={{ marginLeft: "2%" }}><p><a href="https://reactjs.org/" target="_blank" rel="noreferrer" class="link-primary">React.js</a> | <a href="material-ui.com/" target="_blank" rel="noreferrer" class="link-primary">material UI</a> | <a href="https://www.framer.com/motion/" target="_blank" rel="noreferrer" class="link-primary">Framer Motion Animation Library</a> | <a href="https://github.com/obiha/portfolio" target="_blank" rel="noreferrer" class="link-primary">Github Repository</a></p></div>
                                 <div style={{ textAlign: "center" }}> <p style={{ paddingBottom: "3%" }}> &copy; 2022 - Joshua Obiha</p></div>
-                        </div>
+                        </div> */}
 
                 </div>
         )
+
 }
 
 export default Desktop
